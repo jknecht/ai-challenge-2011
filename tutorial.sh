8c693e8b9eda280a87ce253ff7f41f3ef6ed8655
Jeff Knecht <jeff_knecht@yahoo.com>
2011-10-24 23:56:19 -0500

    submission 10/11 to website

--- tutorial.sh
+++ tutorial.sh
@@ -1,4 +1,4 @@
 jar cfm mybot.jar MANIFEST -C bin/ .
 
-python tools/playgame.py "java -jar mybot.jar" "java -jar mybot9.jar" --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns 200 --verbose -e
+python tools/playgame.py "java -jar mybot.jar" "java -jar mybot10.jar" --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns 200 --verbose -e
 
